8bffa691d138422fb28d09afea4aa786e136dfde
Chen Beitian <selena.cbt0102@gmail.com>
2026-01-30 15:40:54 +0800

    Add JavaDoc comments

--- src/main/java/momo/Command.java
+++ src/main/java/momo/Command.java
@@ -4,8 +4,21 @@ package momo;
  * Represents an executable user command.
  */
 public abstract class Command {
+    /**
+     * Executes this command.
+     *
+     * @param tasks The task list.
+     * @param ui The user interface.
+     * @param storage The storage component for saving tasks.
+     * @throws MomoException If an error occurs during command execution.
+     */
     public abstract void execute(TaskList tasks, Ui ui, Storage storage) throws MomoException;
 
+    /**
+     * Returns whether this command should cause the program to exit.
+     *
+     * @return true if this command exits the application, false otherwise.
+     */
     public boolean isExit() {
         return false;
     }
--- src/main/java/momo/TaskList.java
+++ src/main/java/momo/TaskList.java
@@ -13,22 +13,51 @@ public class TaskList {
         this.tasks = new ArrayList<>();
     }
 
+    /**
+     * Constructs a TaskList with an initial list of tasks.
+     *
+     * @param initialTasks The initial tasks to populate the list.
+     */
     public TaskList(List<Task> initialTasks) {
         this.tasks = new ArrayList<>(initialTasks);
     }
 
+    /**
+     * Returns the number of tasks in the list.
+     *
+     * @return The number of tasks.
+     */
     public int size() {
         return tasks.size();
     }
 
+    /**
+     * Adds a task to the task list.
+     *
+     * @param task The task to add.
+     */
     public void add(Task task) {
         tasks.add(task);
     }
 
+    /**
+     * Returns the task at the given index.
+     *
+     * @param index The index of the task.
+     * @return The task at the specified index.
+     * @throws MomoException If the index is invalid.
+     */
     public Task get(int index) {
         return tasks.get(index);
     }
 
+    /**
+     * Deletes and returns the task at the given index.
+     *
+     * @param index The index of the task to delete.
+     * @return The deleted task.
+     * @throws MomoException If the index is invalid.
+     */
     public Task delete(int index) throws MomoException {
         if (index < 0 || index >= tasks.size()) {
             throw new MomoException("Task index is out of range.");
@@ -36,6 +65,11 @@ public class TaskList {
         return tasks.remove(index);
     }
 
+    /**
+     * Returns the list of tasks for storage purposes.
+     *
+     * @return The list of tasks.
+     */
     public List<Task> asListForStorage() {
         return tasks;
     }
